845cf9332a078dd90743e737b4eb6d6897d7c5e1
dev <devpalwar06@gmail.com>
2023-12-28 13:47:40 +0530

    template set-up

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-center p-5 w-[50%] m-auto text-xl">
+      Hello nextjs
+    </div>
+  );
+}
