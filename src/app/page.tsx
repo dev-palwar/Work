fc17226b4c54e4b4c2bfdac76511fdd6395610f8
dev <devpalwar06@gmail.com>
2023-12-28 18:27:39 +0530

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="p-8 w-[50%] m-auto flex justify-center h-[100vh] items-center">
       <Button variant={"default"}>
-        Explore now
+        Testing...
       </Button>
     </div>
   );
